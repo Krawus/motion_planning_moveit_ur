cc44fc5b023c457f45f1fd2e5d4bbca50a23ca60
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2023-05-13 16:25:27 +0000

    added some comments

--- src/motion_planning_moveit.cpp
+++ src/motion_planning_moveit.cpp
@@ -3,43 +3,56 @@
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
 
-int main(int argc, char * argv[])
+// Define planning group
+static const std::string PLANNING_GROUP = "ur_manipulator";
+
+
+int main(int argc, char *argv[])
 {
   // Initialize ROS and create the Node
   rclcpp::init(argc, argv);
   auto const node = std::make_shared<rclcpp::Node>(
-    "motion_planning_moveit",
-    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
-  );
+      "motion_planning_moveit",
+      rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
 
   // Create a ROS logger
   auto const logger = rclcpp::get_logger("motion_planning_moveit");
 
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
-  auto move_group_interface = MoveGroupInterface(node, "ur_manipulator");
+  auto move_group_interface = MoveGroupInterface(node, PLANNING_GROUP);
   // Set a target Pose
-  auto const target_pose = []{
+  auto const target_pose = []
+  {
     geometry_msgs::msg::Pose msg;
-    msg.orientation.w = 1.0;
+    // Point
     msg.position.x = 0.38;
     msg.position.y = -0.4;
     msg.position.z = 0.7;
+    // Quaternion
+    msg.orientation.w = 1.0;
+    msg.orientation.x = 0.0;
+    msg.orientation.y = 0.0;
+    msg.orientation.z = 0.0;
     return msg;
   }();
   move_group_interface.setPoseTarget(target_pose, "wrist_3_link");
 
   // Create a plan to that target pose
-  auto const [success, plan] = [&move_group_interface]{
+  auto const [success, plan] = [&move_group_interface]
+  {
     moveit::planning_interface::MoveGroupInterface::Plan msg;
     auto const ok = static_cast<bool>(move_group_interface.plan(msg));
     return std::make_pair(ok, msg);
   }();
 
   // Execute the plan
-  if(success) {
+  if (success)
+  {
     move_group_interface.execute(plan);
-  } else {
+  }
+  else
+  {
     RCLCPP_ERROR(logger, "Planing failed!");
   }
 
