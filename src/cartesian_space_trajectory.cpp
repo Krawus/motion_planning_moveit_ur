1c0be78d0a33ab0b8820202e9b7a78cf4e4c9ea7
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2023-05-13 16:31:17 +0000

    renamed node to cartesian space trajectory

--- src/cartesian_space_trajectory.cpp
+++ src/cartesian_space_trajectory.cpp
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
   // Initialize ROS and create the Node
   rclcpp::init(argc, argv);
   auto const node = std::make_shared<rclcpp::Node>(
-      "motion_planning_moveit",
+      "cartesian_space_traj",
       rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
 
   // Create a ROS logger
-  auto const logger = rclcpp::get_logger("motion_planning_moveit");
+  auto const logger = rclcpp::get_logger("cartesian_space_traj");
 
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
