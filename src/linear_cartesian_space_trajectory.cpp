b2ba9a1ee9387b06e4165aaf4476b44b0991e91e
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2023-05-13 22:29:08 +0000

    linear cartesian path work in progress

--- src/linear_cartesian_space_trajectory.cpp
+++ src/linear_cartesian_space_trajectory.cpp
@@ -21,30 +21,35 @@ int main(int argc, char *argv[])
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
   auto move_group_interface = MoveGroupInterface(node, PLANNING_GROUP);
-  // Set a target Pose
-  auto const target_pose = []
-  {
-    geometry_msgs::msg::Pose msg;
-    // Point
-    msg.position.x = 0.38;
-    msg.position.y = -0.4;
-    msg.position.z = 0.7;
-    // Quaternion
-    msg.orientation.w = 1.0;
-    msg.orientation.x = 0.0;
-    msg.orientation.y = 0.0;
-    msg.orientation.z = 0.0;
-    return msg;
-  }();
-  move_group_interface.setPoseTarget(target_pose, "wrist_3_link");
 
-  // Create a plan to that target pose
-  auto const [success, plan] = [&move_group_interface]
-  {
-    moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
+  // Create cartesian path
+  std::vector<geometry_msgs::msg::Pose> waypoints;
+  
+  // Getting current pose
+  geometry_msgs::msg::Pose currentPose = move_group_interface.getCurrentPose().pose;
+  waypoints.push_back(currentPose);
+
+  // Setting destination pose 
+  geometry_msgs::msg::Pose destinationPose = currentPose;
+  destinationPose.position.x -=0.4;
+  waypoints.push_back(destinationPose);
+  
+  moveit::planning_interface::MoveGroupInterface::Plan plan;
+  moveit_msgs::msg::RobotTrajectory trajectory;
+  const double jump_threshold = 0.0;
+  const double eef_step = 0.01;
+  double fraction = move_group_interface.computeCartesianPath(waypoints,eef_step,jump_threshold,trajectory);
+
+  bool success = true; // temporary
+  plan.trajectory_ = trajectory;
+
+//   // Create a plan to that target pose
+//   auto const [success, plan] = [&move_group_interface]
+//   {
+//     moveit::planning_interface::MoveGroupInterface::Plan msg;
+//     auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+//     return std::make_pair(ok, msg);
+//   }();
 
   // Execute the plan
   if (success)
